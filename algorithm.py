e5cb608c8216bf05ad6f7f8a5b19776f16d3a3f1
Anne Lin <annelin@annes-mbp.gateway.pace.com>
2015-12-06 16:13:43 -0800

    reverted increment_detected_cycle

--- algorithm.py
+++ algorithm.py
@@ -202,10 +202,10 @@ def increment_detected_cycle(graph, stack, repeated_node, current_node):
 
 
 
-        #if next.label not in cur.edge_cycles.keys():
-            #cur.edge_cycles[next.label] = 1
-        #else:
-            #cur.edge_cycles[next.label] += 1
+    if next.label not in cur.edge_cycles.keys():
+        cur.edge_cycles[next.label] = 1
+    else:
+        cur.edge_cycles[next.label] += 1
 
 
 def get_max_cycle_edge(graph):
